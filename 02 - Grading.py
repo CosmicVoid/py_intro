1a29837a03c10b08452421b1950e2b87eb5630ca
CosmicVoid <98368905+CosmicVoid@users.noreply.github.com>
2022-01-24 20:14:14 -0800

    Add files via upload

---
+++ 02 - Grading.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 19:31:51 2022
+
+@author: Admin
+"""
+
+grade = int(input("Insert Grade: "))
+if grade >= 90:
+    print("You got an A")
+else if grade >= 80:
+    print("You got a B")
+else if grade >= 70:
+    print("You got a C")
+else if grade >= 60:
+    print("You got a D")
+else:
+    print("You got an E")
+    
+    
