f8da166efdfac508e14c00a1d85dbbcb3e66cfd8
CosmicVoid <98368905+CosmicVoid@users.noreply.github.com>
2022-01-24 18:38:27 -0800

    Add files via upload

---
+++ Parameter & Area.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 19:37:58 2022
+
+@author: Admin
+"""
+r = input("Insert radius:")
+print("Parameter:")
+print(float(r)*2.0*3.14)
+print("Area:")
+print(float(r)**2*3.14)
