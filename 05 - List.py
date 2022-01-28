b68e7707c8571b71f3d22b3d560400675757c35a
CosmicVoid <98368905+CosmicVoid@users.noreply.github.com>
2022-01-27 19:49:42 -0800

    Add files via upload

---
+++ 05 - List.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 19:15:35 2022
+
+@author: Admin
+"""
+
+x = []
+x.append('Badminton')
+x.append('Tennis')
+x.append('Basketball')
+x.append('Swimming')
+x.append('School')
+x.insert(2,1)
+x.pop(-1)
+print(x)
