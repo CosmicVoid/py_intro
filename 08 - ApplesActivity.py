2fb9a3519b1cc6105bbd39a15dfe22efb23aca3f
CosmicVoid <98368905+CosmicVoid@users.noreply.github.com>
2022-02-08 19:24:51 -0800

    Add files via upload

--- 08 - ApplesActivity.py
+++ 08 - ApplesActivity.py
@@ -4,13 +4,26 @@ Created on Tue Feb  8 18:22:11 2022
 
 @author: Admin
 """
+def options():
+    print("Options:")
+    print("(1)Input Price and Initial amount")
+    print("(2)Input apples gained")
+    print("(3)Input apples sold")
+    print("(4)Total Earnings")
+    print("(5)Apples Remaining")
+    print("(6)Exit")
+print("vvvvvvvvvvvvvvvvvvvvvv")
+print("Apple Recording System")
+print("^^^^^^^^^^^^^^^^^^^^^^")
+
 while 1==1:
+    options()
     command = int(input("What is the command? "))
     if command==1:
         amount = int(input("What is the initial amount of apples: "))
         cost = int(input("What is the cost of each apple: "))
     elif command==2:
-        stock = int(input("How many apples were stocked: "))
+        stock = int(input("How many apples were gained: "))
     elif command==3:
         sells = int(input("How many apples were sold: "))
     elif command==4:
@@ -21,3 +34,4 @@ while 1==1:
         break
     else:
         print("This is not a command, please choose 1 to 6")
+        options()
